b615785e55848c289013f7fc7d3338fd31ad1958
Chiman-Dev <bchiman.subudhi@gmail.com>
2018-06-10 19:04:29 +0530

    first commit

---
+++ assign.js
@@ -0,0 +1,8 @@
+var temperatureInCelsius = 40;
+var celsiusToFahrenheit = (temperatureInCelsius*9)/5+32; //Formula to convert celsiusToFahrenheit
+console.log("Temperature of " + temperatureInCelsius + " celsius, will be " +celsiusToFahrenheit+ " fahrenheit");
+
+var temperatureInFahrenheit = 104;
+var fahrenheitToCelsius = ((temperatureInFahrenheit-32)*5)/9;//Formula to convert fahrenheitToCelsius
+console.log("Temperature of " + temperatureInFahrenheit + " fahrenheit, will be " +fahrenheitToCelsius+ " celsius");
+
